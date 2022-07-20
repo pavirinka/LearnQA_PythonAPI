b174d661708e91bc1c542eafb30bc1c9ffeebf17
Павлова Ириша <pavirinka@gmail.com>
2022-07-20 12:10:11 +0300

    Выведен текст второго сообщения

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import json
 
-import requests
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
+json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+obj = json.loads(json_text)
+print(obj['messages'][1])
