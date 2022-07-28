da754b44e2fa4169fd744143abbffc301a829cc2
Павлова Ириша <pavirinka@gmail.com>
2022-07-28 21:27:30 +0300

    Добавлен тест на проверку длины введенного текста

--- test_len_text.py
+++ test_len_text.py
@@ -1,5 +1,5 @@
 class TestText:
     def test_check_text(self):
         text = input("Set а text less than 15 characters long: ")
-        assert len(text) < 14, f"Your text has length more then 15 characters"
+        assert len(text) < 15, f"Your text has length more then 15 characters"
 
