4faf6588c3f65a2a9cd77cc73158e64e397d7ac4
Павлова Ириша <pavirinka@gmail.com>
2022-08-01 01:51:13 +0300

    Формирование фреймворка

--- test_test/test_header.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-class TestHeaders:
-    def test_headers(self):
-        response = requests.get("https://playground.learnqa.ru/api/homework_header")
-        resp_headers = response.headers
-        print(resp_headers)
-        assert response.headers.get("x-secret-homework-header") == "Some secret value","There is no headers  'x-secret-homework-header': 'Some secret value' in response"
-
